1aa85e8a498c896baa846c69f18bd62260874b33
gamashbogdan <gamashbogdan@gmail.com>
2024-01-21 17:19:14 +0100

    1 - Зробив дз втікаюча кнопка

--- 01_Moving_Button/Form1.Designer.cs
+++ 01_Moving_Button/Form1.Designer.cs
@@ -28,13 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.labelText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.BackColor = System.Drawing.Color.OrangeRed;
+            this.buttonNo.Font = new System.Drawing.Font("Segoe Print", 10.2F);
+            this.buttonNo.Location = new System.Drawing.Point(404, 76);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(145, 44);
+            this.buttonNo.TabIndex = 5;
+            this.buttonNo.Text = "No";
+            this.buttonNo.UseVisualStyleBackColor = false;
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.BackColor = System.Drawing.Color.Chartreuse;
+            this.buttonYes.Font = new System.Drawing.Font("Segoe Print", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonYes.Location = new System.Drawing.Point(247, 76);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(145, 44);
+            this.buttonYes.TabIndex = 4;
+            this.buttonYes.Text = "Yes";
+            this.buttonYes.UseVisualStyleBackColor = false;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Font = new System.Drawing.Font("Segoe Print", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelText.Location = new System.Drawing.Point(238, 9);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(311, 49);
+            this.labelText.TabIndex = 3;
+            this.labelText.Text = "I like going to school";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.Controls.Add(this.labelText);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonNo;
+        private System.Windows.Forms.Button buttonYes;
+        private System.Windows.Forms.Label labelText;
     }
 }
 
--- 01_Moving_Button/Form1.cs
+++ 01_Moving_Button/Form1.cs
@@ -16,5 +16,55 @@ namespace _01_Moving_Button
         {
             InitializeComponent();
         }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = $"Mouse position : {e.X} : {e.Y}";
+            Point mouse = e.Location;
+            Random random = new Random();
+            if (mouse.X >= buttonNo.Left - 20 && (mouse.X <= buttonNo.Left + buttonNo.Width + 20))
+            {
+                if (mouse.X >= buttonNo.Left + (buttonNo.Width / 2))
+                {
+                    buttonNo.Left = buttonNo.Left - 30;
+                }
+                else
+                {
+                    buttonNo.Left = buttonNo.Left + 30;
+                }
+            }
+            else if (mouse.Y >= buttonNo.Top + 20 && (mouse.Y <= buttonNo.Top + buttonNo.Height + 20))
+            {
+                if (mouse.Y >= buttonNo.Top + (buttonNo.Width / 2))
+                {
+                    buttonNo.Top = buttonNo.Top - 20;
+                }
+                else
+                {
+                    buttonNo.Top = buttonNo.Top + 20;
+                }
+
+            }
+            if (buttonNo.Top < 0)
+            {
+                buttonNo.Location = new Point(random.Next(this.Width), random.Next(this.Height));
+            }
+            if (buttonNo.Top + buttonNo.Height > this.ClientSize.Height)
+            {
+                buttonNo.Location = new Point(random.Next(this.Width), random.Next(this.Height));
+            }
+            if (buttonNo.Left < 0)
+            {
+                buttonNo.Location = new Point(random.Next(this.Width), random.Next(this.Height));
+            }
+            if (buttonNo.Left + buttonNo.Width > this.ClientSize.Width)
+            {
+                buttonNo.Location = new Point(random.Next(this.Width), random.Next(this.Height));
+            }
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(":))))))))))))))))))))))))))", "Good ;D", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
